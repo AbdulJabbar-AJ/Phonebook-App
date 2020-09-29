35e0250332a930bb263ae4506ccd4cf58a4e313e
Abdul Jabbar <abdulabase@gmail.com>
2020-09-29 03:20:08 +0100

    bug fix

--- src/scripts/views/sections/contactsView.jsx
+++ src/scripts/views/sections/contactsView.jsx
@@ -25,7 +25,7 @@ const ContactsView = ({contacts, activeContact, topContact, showContact, narrowV
 	function showCard() {
 		if (!isNewContact) {
 			if (!activeContact) {
-				if (!narrowView && topContact !== '') {
+				if (!narrowView && topContact !== '' && contacts.some(contact => contact.id === topContact)) {
 					setContact(contacts.find(contact => contact.id === topContact))
 				} else {
 					setContact({})
@@ -60,6 +60,7 @@ const ContactsView = ({contacts, activeContact, topContact, showContact, narrowV
 
 	const side = <div className='sidePanel'><ContactsList {...{contacts, activeContact: contactObject, showContactCallback: activateContact}} /></div>
 
+	console.log(contactObject)
 	const main = contactObject.hasOwnProperty('id')
 		? <div className={classNames('mainPanel', {slideLeft})}><ContactCard {...{contact: contactObject, isNewContact, setIsNewContact, closeContactCallback: () => showContact('')}}/></div>
 		: <BlankMessage message='No Contact Selected'/>
